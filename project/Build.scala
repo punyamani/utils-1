97f3fc819711e1408a8b26eee4a87f41b48f8125
Kinshuk Bairagi <me@kinshuk.in>
2016-02-09 14:26:01 +0530

    Creating Project Structure

---
+++ project/Build.scala
@@ -0,0 +1,112 @@
+import sbt.Keys._
+import sbt._
+
+object Util extends Build {
+  	val branch = Process("git" :: "rev-parse" :: "--abbrev-ref" :: "HEAD" :: Nil).!!.trim
+  	val suffix = if (branch == "master") "" else "-SNAPSHOT"
+  
+  	val libVersion = "0.0.1" + suffix
+
+
+	def scalacOptionsVersion(sv: String): Seq[String] = {
+	    Seq(
+	      // Note: Add -deprecation when deprecated methods are removed
+	      "-unchecked",
+	      "-feature",
+	      "-encoding", "utf8"
+	    ) ++ (CrossVersion.partialVersion(sv) match {
+	      // Needs -missing-interpolator due to https://issues.scala-lang.org/browse/SI-8761
+	      case Some((2, x)) if x >= 11 => Seq("-Xlint:-missing-interpolator")
+	      case _ => Seq("-Xlint")
+	    })
+  	}
+
+  	 val sharedSettings = Seq(
+    version := libVersion,
+    organization := "com.flipkart",
+    scalaVersion := "2.11.7",
+    crossScalaVersions := Seq("2.10.6", "2.11.7"),
+    // Workaround for a scaladoc bug which causes it to choke on empty classpaths.
+    unmanagedClasspath in Compile += Attributed.blank(new java.io.File("doesnotexist")),
+    libraryDependencies ++= Seq(
+      "junit" % "junit" % "4.8.1" % "test",
+      "org.mockito" % "mockito-all" % "1.9.5" % "test",
+      "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+    ),
+
+   
+    scalacOptions := scalacOptionsVersion(scalaVersion.value),
+
+    // Note: Use -Xlint rather than -Xlint:unchecked when TestThriftStructure
+    // warnings are resolved
+    javacOptions ++= Seq("-Xlint:unchecked", "-source", "1.7", "-target", "1.7"),
+    javacOptions in doc := Seq("-source", "1.7"),
+
+    // This is bad news for things like com.twitter.util.Time
+    parallelExecution in Test := false,
+
+    // Sonatype publishing
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    publishMavenStyle := true,
+    autoAPIMappings := true,
+    apiURL := Some(url("https://github.com/Flipkart/utils")),
+    pomExtra :=
+      <url>https://github.com/Flipkart/utils</url>
+      <licenses>
+        <license>
+          <name>Apache License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0</url>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:Flipkart/utils</url>
+        <connection>scm:git:git@github.com:Flipkart/utils</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>Flipkart</id>
+          <name>Flipkart Internet Pvt. Ltd.</name>
+          <url>https://www.flipkart.com/</url>
+        </developer>
+      </developers>,
+    publishTo <<= version { (v: String) =>
+      val flipkart = "http://artifactory.nm.flipkart.com:8081/artifactory/"
+	  if (v.trim.endsWith("SNAPSHOT"))
+	    Some("Flipkart Repo Snapshots" at flipkart + "libs-snapshot-local")
+	  else
+	    Some("Flipkart Repo Releases"  at flipkart + "libs-release-local")
+    }
+
+   
+  )
+
+
+	lazy val util = Project(
+    id = "utils",
+    base = file("."),
+    settings = Defaults.coreDefaultSettings ++
+      sharedSettings 
+  ) aggregate(
+    utilCore, utilConfig
+  )
+
+lazy val utilCore = Project(
+    id = "util-core",
+    base = file("util-core"),
+    settings = Defaults.coreDefaultSettings ++
+      sharedSettings
+  ).settings(
+    name := "util-core"
+  )
+
+  lazy val utilConfig = Project(
+    id = "util-config",
+    base = file("util-config"),
+    settings = Defaults.coreDefaultSettings ++
+      sharedSettings
+  ).settings(
+    name := "util-config"
+  ).dependsOn(utilCore)
+
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,8 @@
+logLevel := Level.Warn
+
+resolvers += Resolver.url("artifactory", url("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+resolvers += Classpaths.sbtPluginReleases
+
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.12.0")
